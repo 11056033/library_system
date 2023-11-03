ffcab1cb6c5649eff030d0f2dfdd2f49a3ceb5ec
1105603326 <jimmy2005.tw@gmail.com>
2023-11-03 17:32:55 +0800

    almost done

--- mysite/models.py
+++ mysite/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    introduce = models.CharField(max_length=200, default="None")
     author = models.CharField(max_length=200, default="不詳" )
     slug = models.CharField(max_length=200)
     body = models.TextField()
