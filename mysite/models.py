60df923bec1a91ebc8563ee33182e10d252a3c5d
1105603326 <jimmy2005.tw@gmail.com>
2023-11-03 16:46:11 +0800

    更改書籍列表

--- mysite/models.py
+++ mysite/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200, default="不詳")
+    author = models.CharField(max_length=200, default="不詳" )
     slug = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
