52dc63926e3d530c1935c6de81502acb12148a0f
1105603326 <jimmy2005.tw@gmail.com>
2024-01-06 17:17:33 +0800

    homo

--- mblog/settings.py
+++ mblog/settings.py
@@ -27,11 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 # Application definition
@@ -43,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite'
+    'mysite',
+    'mytest'
 ]
 
 MIDDLEWARE = [
@@ -123,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- mblog/urls.py
+++ mblog/urls.py
@@ -17,8 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mysite import views as mv
+from mytest import views as testv
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',mv.homepage,name="homepage"),
-    path('post/<slug:slug>/',mv.showpost,name='showpost')
+    path('', mv.homepage, name="homepage"),
+    path('post/<slug:slug>/', mv.showpost, name="showpost"),
+    path('post/', mv.show_all_posts, name="show-all-posts"),
+    path('post/<int:post_id>/comments', mv.show_comments, name='show-comments'),
+    path('about/', mv.about),
+    path('about/<int:num>', mv.about, name='about'),
+    path('carlist/', mv.carlist),
+    path('carlist/<int:maker>/', mv.carlist, name='carlist-url'),
+    path('post/new', mv.new_post, name="post-new"),
+    path('test/', testv.index, name="test-new"),
+    path('test/delpost/<int:pid>/', testv.delpost),
+    path('test/contact', testv.contact),
+    path('post2db/', testv.post2db),
+    path('register/', testv.register),
+    path('login/', testv.login, name='login'),
+    path('logout/', testv.logout_view, name='logout'),
+    path('profile/', testv.profile),
+    path('logout/', testv.user_logout, name='logout'),
+    path('user_posts/', testv.user_posts, name='user_posts'),
+    path('edit_post/<int:post_id>/', testv.edit_post, name='edit_post'),
+    path('login/register/', testv.register, name='register'),
+    path('delete_post/<int:post_id>/', testv.delete_post, name='delete_post')
 ]
